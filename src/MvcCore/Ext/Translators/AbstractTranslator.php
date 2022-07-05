63b5a4d28f647c679ec0e3f4e589c418cd64a274
Tom Flídr <tomflidr@gmail.com>
2022-07-05 14:04:43 +0200

    release v5.0.6

--- src/MvcCore/Ext/Translators/AbstractTranslator.php
+++ src/MvcCore/Ext/Translators/AbstractTranslator.php
@@ -27,7 +27,7 @@ abstract class AbstractTranslator implements \MvcCore\Ext\ITranslator {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.0.5';
+	const VERSION = '5.0.6';
 
 	/**
 	 * Singleton instace for each localization.
